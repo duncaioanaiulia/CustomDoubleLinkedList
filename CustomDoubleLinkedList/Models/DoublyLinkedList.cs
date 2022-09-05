fa103f5e8ef089d3dc34886df72c57c6eb8ea76a
Ioana Dunca <dunca.ioana.iulia@gmail.com>
2022-09-05 16:22:04 +0300

    Create Module for Node and CustomDoubleLinkedList

---
+++ CustomDoubleLinkedList/Models/DoublyLinkedList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDoubleLinkedList.Models
+{
+    internal class DoublyLinkedList<T>
+    {
+        // this Node class is created in DoublyLinkedList for easy reference
+        internal class Node
+        {
+            internal T Value { get; set; }
+            internal Node Previous { get; set; }
+            internal Node Next { get; set; }
+
+            public Node(T t)
+            {
+                Value = t;
+                Previous = null;
+                Next = null;
+            }
+        }
+
+        private Node _head;
+
+        public Node First => _head;
+
+        public Node Last
+        {
+            get
+            {
+                Node node = GetLastNode();
+                return node;
+            }
+
+        }
+
+        public Node Next => _head.Next;
+        public Node Previous => _head.Previous;
+
+        private DoublyLinkedList<T>.Node GetLastNode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
